fc8b6b2df569f36e497466dc771f8b4d4ee490e2
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-24 21:18:03 -0300

    Retornar 404 caso não exista a Categoria

--- src/main/java/com/tarcisiolopes/apifinancas/resource/CategoriaResource.java
+++ src/main/java/com/tarcisiolopes/apifinancas/resource/CategoriaResource.java
@@ -43,8 +43,12 @@ public class CategoriaResource {
 	}
 	
 	@GetMapping("/{codigo}")
-	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
-		return categoriaRepository.findById(codigo);
+	public ResponseEntity<Optional<Categoria>> buscarPeloCodigo(@PathVariable Long codigo) {
+		 Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		 if (categoria != null)
+			return ResponseEntity.ok(categoria);
+		else
+			return ResponseEntity.notFound().build();
 	}
 	
 }
